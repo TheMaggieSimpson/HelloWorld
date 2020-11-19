45005e889de9a20c82bfc049d1b6224897aebfa1
Rosanny <TheMaggieSimpson@users.noreply.github.com>
2020-11-19 21:20:20 +0100

    update security

--- ExpressJS-HTML-Pug/index.js
+++ ExpressJS-HTML-Pug/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const helmet = require('helmet')
 
 const app = express()
 const port = 3000
+
+app.use(helmet())
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'pug')
